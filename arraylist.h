4e0d201d5593f60d8080f650d3e3fad0930b2656
randy <randy@sevron.co.uk>
2023-09-24 18:05:04 +0800

    solved get and add only

--- arraylist.h
+++ arraylist.h
@@ -10,23 +10,45 @@ class ArrayList : public List {
     int size;
     int capacity = 5;
 
+    void dynamic_add() {
+        double new_size = ceil(capacity * 1.5);
+        double* new_array = (double*)realloc(array, sizeof(double) * new_size);
+        array = new_array;
+        capacity = new_size;
+    }
+
+    void dynamic_deduce() {
+        double new_size = ceil(capacity * 0.75);
+        double* new_array = (double*)realloc(array, sizeof(int) * new_size);
+        array = new_array;
+        capacity = new_size;
+    }
+
 public:
     // constructor
     ArrayList() {
         // initialize array using malloc or calloc here
+        array = (double*)calloc(capacity, sizeof(double));
         size = 0;
     }
 
     void add(double num) {
         // TODO implement add with dynamic array
+        if (size == capacity) {
+            dynamic_add();
+        }
+        array[size++] = num;
     }
 
     int remove(double num) {
         // TODO implement remove with dynamic array
+
+        return -1;
     }
 
     double get(int pos) {
         // TODO implement get
+        return array[pos -1];
     }
 
     void print() {
